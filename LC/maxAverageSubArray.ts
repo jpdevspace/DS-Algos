d308ef773de47765e2ba9b1cdf1432397bb053aa
Jean Paul G <jpgiraldo7@gmail.com>
2024-01-01 22:04:21 -0500

    Working on a v2

--- LC/maxAverageSubArray.ts
+++ LC/maxAverageSubArray.ts
@@ -16,6 +16,26 @@ const findMaxAverage = (nums: number[], k: number): number => {
 }
 
 
+const findMaxAverage2 = (nums: number[], k: number): number => {
+  let sum = 0;
+  let maxAverage = Number.NEGATIVE_INFINITY;
+
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+  
+  maxAverage = sum / k;
+  
+  for (let j = 1; j < nums.length - k; j++) {
+    const newSum = sum - nums[j-1] + nums[k+1];
+    if ((newSum / k) > maxAverage) {
+      maxAverage = (newSum / k);
+    }
+  }
+
+  return maxAverage;
+}
+
 const nums1 = [1,12,-5,-6,50,3];
 const k1 = 4;
 
@@ -29,3 +49,6 @@ console.log('findMaxAverage (1) >>> ', findMaxAverage(nums1, k1));
 console.log('findMaxAverage (2) >>> ', findMaxAverage(nums2, k2));
 console.log('findMaxAverage (3) >>> ', findMaxAverage(nums3, k3));
 
+console.log('findMaxAverage2 (1) >>> ', findMaxAverage2(nums1, k1));
+console.log('findMaxAverage2 (2) >>> ', findMaxAverage2(nums2, k2));
+console.log('findMaxAverage2 (3) >>> ', findMaxAverage2(nums3, k3));
